Fixes rounding error which caused masking to fail, adds command line flags for ai

ai.py
@@ -1,7 +1,8 @@
-from minesweeping import gen_board, get_mine_counts, select
+from minesweeping import gen_board, get_neighbor_counts, select
 from nd_minesweeper import print_known
 import numpy as np
 from random import choice
+import argparse
 
 
 def select_random_unknown_index(known, marked):
@@ -10,53 +11,73 @@ def select_random_unknown_index(known, marked):
 
 
 def select_safe_indices(known, marked):
-    marked_counts = get_mine_counts(marked)
+    marked_counts = get_neighbor_counts(marked)
     satisfied = (marked_counts == known) & (known > 0)
-    safe_indices = get_mine_counts(satisfied) * (known < 0) * (marked < 1)
+    safe_indices = get_neighbor_counts(satisfied) * (known < 0) * (marked < 1)
     return np.transpose(np.nonzero(safe_indices))
 
 
 def mark_mines(known, marked):
-    unknown = get_mine_counts(known < 0)
+    unknown = get_neighbor_counts(known < 0)
     satisfied = (unknown == known) & (known > 0)
-    new_marked = np.round(get_mine_counts(satisfied) * (known < 0))
+    new_marked = np.round(get_neighbor_counts(satisfied) * (known < 0))
     marked[(marked + new_marked) > 0] = 1
 
 
-def solve(board, counts, known):
+def solve(board, serial=False):
+    counts = get_neighbor_counts(board)
+    known = np.ones(dims) * -1
     marked = np.zeros(known.shape)
+
     prev_known, prev_marked = None, None
+    turns = 0
     while True:
+        turns += 1
         if np.sum(prev_known) == np.sum(known)\
                 and np.sum(prev_marked) == np.sum(marked):
             index = select_random_unknown_index(known, marked)
             safe = select(index, board, counts, known)
             if not safe:
                 print('You lose! (random selection)')
-                break
+                return False
         prev_known = known.copy()
         prev_marked = marked.copy()
         mark_mines(known, marked)
         indices = select_safe_indices(known, marked)
-        for index in indices[:1]:
-            safe = select(index, board, counts, known)
-            if not safe:
-                print('You lose! (assertion fail)')
-                break
-        print(known)
+        if serial:
+            if len(indices) > 0:
+                safe = select(indices[0], board, counts, known)
+        else:
+            for index in indices:
+                safe = select(index, board, counts, known)
+                assert safe
+        print_known(known, marked=marked)
         assert np.sum((known > 0) * board) == 0
         print(np.sum(marked))
         if np.sum(board) == np.sum(marked):
             assert np.sum(board * marked) == np.sum(board)
-            print('You win!')
-            break
+            print('Game won in {} turns!'.format(turns))
+            return True
 
 
-if __name__ == "__main__":
-    # Figure out why it works (okay) for 20x25, but totally breaks for 20x26
-    dims = [20, 20]
-    board = gen_board(dims, 50)
-    counts = get_mine_counts(board)
-    known = np.ones(dims) * -1
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Plays minesweeper on arbitray n-dimensional grids")
+    parser.add_argument('-d', '--dims', nargs='+', help='List of dimensions')
+    parser.add_argument('-m', '--mines', help='Number of mines')
+    parser.add_argument('-u', '--until_win', help='Play until a win')
+    parser.add_argument('-s', '--serial', help='Take one turn at a time')
 
-    solve(board, counts, known)
+    args = parser.parse_args()
+    dims = tuple(map(int, args.dims)) if args.dims else [16, 30]
+    mines = int(args.mines) if args.mines else 99
+    until_win = True if args.until_win in ['True', 'true', '1'] else False
+    serial = True if args.serial in ['True', 'true', '1'] else False
+
+    if until_win:
+        tries = 1
+        while not solve(gen_board(dims, mines), serial=serial):
+            tries += 1
+        print('Game won in {} tries.'.format(tries))
+    else:
+        solve(gen_board(dims, mines), serial=serial)

minesweeper.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from minesweeping import gen_board, get_mine_counts, select
+from minesweeping import gen_board, get_neighbor_counts, select
 import numpy as np
 import curses
 import re
@@ -42,7 +42,7 @@ def display_screen(screen_string, screen, colors, offset_x=0, offset_y=0):
 
 def play_game(dims, mines, screen):
     board = gen_board(dims, mines)
-    counts = get_mine_counts(board)
+    counts = get_neighbor_counts(board)
     known = np.ones(dims) * -1
     flags = np.zeros(dims)
 
@@ -127,8 +127,8 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--mines', help='Number of mines')
 
     args = parser.parse_args()
-    dims = tuple(map(int, args.dims)) if args.dims is not None else [16, 30]
-    mines = int(args.mines) if args.mines is not None else 99
+    dims = tuple(map(int, args.dims)) if args.dims else [16, 30]
+    mines = int(args.mines) if args.mines else 99
 
     key = ''
     while key != 'q':

minesweeping.py
@@ -13,7 +13,7 @@ def gen_board(dims, mines):
     return board.reshape(dims)
 
 
-def get_mine_counts(board):
+def get_neighbor_counts(board):
     # Gets the count of mines in all cells' neighbors by using an n-dimensional
     # convolution with a unit tensor in the appropriate dimension
     kernel = np.ones([3] * len(board.shape))
@@ -38,6 +38,7 @@ def select(index, board, counts, known):
         connected_components = label(1 - (counts > 0), structure=kernel)[0]
         empty_mask = connected_components == connected_components[index]
         dims = list(map(lambda d: slice(1, d + 1), board.shape))
-        full_mask = convolve(kernel, 1 * empty_mask)[dims] > 0
-        known[full_mask] = counts[full_mask]
+        new_known = convolve(kernel, 1 * empty_mask)[dims]
+        known_mask = np.round(np.abs(new_known)) > 0
+        known[known_mask] = counts[known_mask]
         return True

nd_minesweeper.py
@@ -3,15 +3,18 @@ import re
 import argparse
 
 import numpy as np
-from minesweeping import get_mine_counts, gen_board, select
+from minesweeping import get_neighbor_counts, gen_board, select
 
 
-def print_known(known):
+def print_known(known, marked=None):
     unknown_mask = known == -1
     zero_mask = known == 0
     array_string = known.astype('U1')
     array_string[unknown_mask] = '■'
     array_string[zero_mask] = ' '
+    if marked is not None:
+        marked_mask = marked == 1
+        array_string[marked_mask] = '*'
     array_string = np.array2string(array_string, max_line_width=1000)
     print(re.sub(r'\'| *\[+|\]+', '', array_string))
 
@@ -27,7 +30,7 @@ def print_game_over(board, counts):
 
 def play_game(dims, mines):
     board = gen_board(dims, mines)
-    counts = get_mine_counts(board)
+    counts = get_neighbor_counts(board)
     known = np.ones(dims) * -1
 
     while True:
@@ -57,8 +60,8 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--mines', help='Number of mines')
 
     args = parser.parse_args()
-    dims = tuple(map(int, args.dims)) if args.dims is not None else [16, 30]
-    mines = int(args.mines) if args.mines is not None else 99
+    dims = tuple(map(int, args.dims)) if args.dims else [16, 30]
+    mines = int(args.mines) if args.mines else 99
 
     while True:
         try: